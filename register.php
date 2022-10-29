b93a5ae58a67684238945f6a7738bbfc8d4a4171
Myron <josephonyekachi77@gmail.com>
2022-10-28 21:07:58 -0800

    updated

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php 
+    include 'path.php';
     include(ROOT_PATH . '/app/includes/header.php')
 ?>
 
@@ -11,45 +12,44 @@
           <div id="contact" class="box-shadow-full">
             <div class="title-box-2">
               <h5 class="title-left1 ">
-                Send Message Us
+                Register
               </h5>
             </div>
             <div>
               <form action="" method="post" class="contactForm">
-                <div id="sendmessage">Your message has been sent. Thank you!</div>
-                <div id="errormessage"></div>
+
+
                 <div class="row">
+
                   <div class="col-md-12 mb-3">
                     <div class="form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
-                        data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                      <div class="validation"></div>
+                      <input type="text" name="username" class="form-control" id="name" placeholder="Username"/>
                     </div>
                   </div>
+
                   <div class="col-md-12 mb-3">
                     <div class="form-group">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email"
-                        data-rule="email" data-msg="Please enter a valid email" />
-                      <div class="validation"></div>
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email"/>
                     </div>
                   </div>
+
                   <div class="col-md-12 mb-3">
                     <div class="form-group">
-                      <input type="text" class="form-control" name="number" id="subject" placeholder="Mobile Number"
-                        data-rule="minlen:4" data-msg="Please enter mobile number" />
-                      <div class="validation"></div>
+                      <input type="text" class="form-control" name="password" id="subject" placeholder="Enter Password"/>
                     </div>
                   </div>
+
                   <div class="col-md-12 mb-3">
                     <div class="form-group">
-                      <textarea class="form-control" name="message" rows="5" data-rule="required"
-                        data-msg="Please write something for us" placeholder="Message"></textarea>
-                      <div class="validation"></div>
+                      <input type="text" class="form-control" name="confirm_password" id="subject" placeholder="Confirm Your Password"/>
                     </div>
                   </div>
+                 
+
                   <div class="col-md-12">
-                    <button type="submit" class="btnn py-2">Send Message</button>
+                    <button type="submit" name="register-btn" class="btnn py-2">Register</button>
                   </div>
+
                 </div>
               </form>
             </div>
@@ -58,3 +58,6 @@
       </div>
     </section>
   <!--/ Section Blog End /-->
+<?php 
+    include ROOT_PATH . '/app/includes/footer.php'
+?>
