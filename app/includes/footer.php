c5457eb469f5b397389a0ff82836a5d5c111bd9a
Myron <josephonyekachi77@gmail.com>
2022-10-28 20:56:14 -0800

    updated

---
+++ app/includes/footer.php
@@ -0,0 +1,60 @@
+<!--/ Section Contact-Footer Star /-->
+<footer>
+    <div class="container-fluid bg-dark pb-2">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="socials">
+            <ul>
+              <li><a href=""><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+              <!-- <li><a href=""><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li> -->
+              <li><a href=""><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
+              <li><a href=""><span class="ico-circle"><i class="ion-social-linkedin"></i></span></a></li>
+              <li><a href=""><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
+            </ul>
+          </div>
+          <div class="copyright-box">
+            <p class="copyright">&copy; Copyright <strong>KamTech</strong>. All Rights Reserved</p>
+            <div class="credits">
+              Designed by <a href="#">Kam-Tech</a>
+            </div>
+          </div>
+  
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <a class="back-to-top" href="#"><i class="fa fa-chevron-up"></i></a>
+    <div id="preloader"></div>
+  
+   
+    <!-- JavaScript Libraries -->
+    <script src="assets/lib/jquery/jquery.min.js"></script>
+    <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+    <script src="assets/lib/popper/popper.min.js"></script>
+    <script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/lib/easing/easing.min.js"></script>
+    <!-- <script src="assets/lib/counterup/jquery.waypoints.min.js"></script> -->
+    <script src="assets/lib/counterup/jquery.counterup.js"></script>
+    <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets/lib/lightbox/js/lightbox.min.js"></script>
+    <script src="assets/lib/typed/typed.min.js"></script>
+    <script src="assets/lib/dist/aos.js"></script>
+
+    <!-- Contact Form JavaScript File -->
+    <script src="assets/lib/contactform/contactform.js"></script>
+
+    <!-- Template Main Javascript File -->
+    <script src="assets/js/main.js"></script>
+  
+    <script>
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out-sine'
+  
+      });
+    </script>
+  
+</body>
+
+</html>
