98836827ef5fedd422d4af6874ee0e0a1610c037
Myron <josephonyekachi77@gmail.com>
2022-11-03 15:58:16 -0800

    updated

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -26,9 +26,8 @@ require_once(ROOT_PATH . '/app/controllers/post.php');
                         <th scope="col">Title</th>
                         <th scope="col">Category</th>
                         <th scope="col">Section</th>
-                        <th scope="col">State</th>
                         <th scope="col">Action</th>
-                        
+                        <th scope="col">State</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -47,18 +46,17 @@ require_once(ROOT_PATH . '/app/controllers/post.php');
                             </td>
 
                             <td>
+                                <a href="edit.php?id=<?php echo $post['id'] ?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+
+                                <a href="" class="btn btn-sm btn-danger">Delete</a>   
+                            </td>
+
+                            <td>
                                 <?php if($post['published']): ?>
                                     <a href="edit.php?published=0&p_id=<?php echo $post['id'] ?>" class="">Unpublish</a>
                                 <?php else: ?>
                                     <a href="edit.php?published=1&p_id=<?php echo $post['id'] ?>" class="">Publish</a>
                                 <?php endif; ?>
-                            </td>
-
-                            
-                            <td>
-                                <a href="" class="btn btn-sm btn-primary mr-1">Edit</a>
-
-                                <a href="" class="btn btn-sm btn-danger">Delete</a>   
                             </td>
                         </tr>
                     <?php endforeach;?>
