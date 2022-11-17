3f71d38f28d9e3f4a98050dca3164517ae531fbe
Myron <josephonyekachi77@gmail.com>
2022-11-16 18:28:24 -0800

    updated

--- admin/skills/index.php
+++ admin/skills/index.php
@@ -5,48 +5,48 @@ require_once(ROOT_PATH . '/admin/includes/adminheader.php');
 require_once(ROOT_PATH . '/app/controllers/skill.php');
 adminOnly();
 ?>
-    <section style="display: flex;">
-        <div class="lside">
+<section style="display: flex;">
+    <div class="lside">
         <?php
-            require_once(ROOT_PATH . '/admin/includes/adminsidebar.php');
+        require_once(ROOT_PATH . '/admin/includes/adminsidebar.php');
         ?>
+    </div>
+
+    <div class="rside">
+        <div class="float-right">
+            <a class="btn btn-primary" href="create.php">Add Skill</a>
+        </div>
+        <div class="adduser-sec">
+            <h2>Manage Skills</h2>
         </div>
 
-        <div class="rside">
-            <div class="float-right">
-                <a class="btn btn-primary" href="create.php">Add Skill</a>
-            </div>
-            <div class="adduser-sec">
-                <h2>Manage Skills</h2>
-            </div>
-
-            <table class="table mt-3">
-                <thead>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">S/N</th>
+                    <th scope="col">Skills</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($skills as $key => $skill) : ?>
                     <tr>
-                        <th scope="col">S/N</th>
-                        <th scope="col">Skills</th>
-                        <th scope="col">Rating</th>
-                        <th scope="col">Action</th>
+                        <th scope="row"><?php echo $key + 1 ?></th>
+                        <td><?php echo $skill['skill'] ?></td>
+                        <td><?php echo $skill['rating'] ?>%</td>
+                        <td>
+                            <a href="edit.php?id=<?php echo $skill['id'] ?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+
+                            <a href="index.php?del_id=<?php echo $skill['id'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure...?')">Delete</a>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($skills as $key => $skill): ?>
-                        <tr>
-                            <th scope="row"><?php echo $key + 1?></th>
-                            <td><?php echo $skill['skill']?></td>
-                            <td><?php echo $skill['rating']?>%</td>
-                            <td>
-                                <a href="edit.php?id=<?php echo $skill['id']?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
-                                <a href="index.php?del_id=<?php echo $skill['id']?>" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure...?')">Delete</a>   
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-
-        </div>
-    </section>
+    </div>
+</section>
 
 </body>
 
