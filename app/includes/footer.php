fb0ffbca5ed1c7506ad32d8bc018ad352c6fdd2b
Myron <josephonyekachi77@gmail.com>
2022-12-05 22:21:45 -0800

    updated

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,55 +1,55 @@
 <!--/ Section Contact-Footer Star /-->
 <footer>
-    <div class="container-fluid bg-dark pb-2">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="socials">
-            <ul>
-              <li><a href="https://web.facebook.com/myron.joe.50"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
-              <li><a href="https://twitter.com/kamTech_01"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
-              <li><a href="https://wa.me/2347046617413?text=WELCOME%20to%20Myron%20Tech.%20We%20offer%20Web%20Design,%20Web%20Development,%20Graphics%20Design%20and%20more."><span class="ico-circle"><i class="ion-social-whatsapp"></i></span></a></li>
-              <li><a href="https://github.com/MyronJoe"><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
-            </ul>
-          </div>
-          <div class="copyright-box">
-            <p class="copyright">&copy; Copyright <strong>KamTech</strong>. All Rights Reserved</p>
-            <div class="credits">
-              Designed by <a href="#">Kam-Tech</a>
-            </div>
-          </div>
-  
+  <div class="container-fluid bg-dark pb-2">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="socials">
+          <ul>
+            <li><a href="https://web.facebook.com/myron.joe.50"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+            <li><a href="https://twitter.com/kamTech_01"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
+            <li><a href="https://wa.me/2347046617413?text=WELCOME%20to%20Myron%20Tech.%20We%20offer%20Web%20Design,%20Web%20Development,%20Graphics%20Design%20and%20more."><span class="ico-circle"><i class="ion-social-whatsapp"></i></span></a></li>
+            <li><a href="https://github.com/MyronJoe"><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
+          </ul>
         </div>
+        <div class="copyright-box">
+          <p class="copyright">&copy; Copyright <strong>KamTech</strong>. All Rights Reserved</p>
+          <div class="credits">
+            Designed by <a href="#">Kam-Tech</a>
+          </div>
+        </div>
+
       </div>
     </div>
-  </footer>
-  
-  <a class="back-to-top" href="#"><i class="fa fa-chevron-up"></i></a>
-    <div id="preloader"></div>
-  
-   
-    <!-- JavaScript Libraries -->
-    <script src="assets/lib/jquery/jquery.min.js"></script>
-    <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
-    <script src="assets/lib/popper/popper.min.js"></script>
-    <script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/lib/easing/easing.min.js"></script>
-    <script src="assets/lib/counterup/jquery.counterup.js"></script>
-    <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="assets/lib/lightbox/js/lightbox.min.js"></script>
-    <script src="assets/lib/typed/typed.min.js"></script>
-    <script src="assets/lib/dist/aos.js"></script>
+  </div>
+</footer>
 
-    <!-- Template Main Javascript File -->
-    <script src="assets/js/main.js"></script>
-  
-    <script>
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out-sine'
-  
-      });
-    </script>
-  
+<a class="back-to-top" href="#"><i class="fa fa-chevron-up"></i></a>
+<div id="preloader"></div>
+
+
+<!-- JavaScript Libraries -->
+<script src="assets/lib/jquery/jquery.min.js"></script>
+<script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+<script src="assets/lib/popper/popper.min.js"></script>
+<script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/lib/easing/easing.min.js"></script>
+<script src="assets/lib/counterup/jquery.counterup.js"></script>
+<script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="assets/lib/lightbox/js/lightbox.min.js"></script>
+<script src="assets/lib/typed/typed.min.js"></script>
+<script src="assets/lib/dist/aos.js"></script>
+
+<!-- Template Main Javascript File -->
+<script src="assets/js/main.js"></script>
+
+<script>
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out-sine'
+
+  });
+</script>
+
 </body>
 
 </html>
