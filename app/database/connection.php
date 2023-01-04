3ad9c9a95a0601e2108e6f19b04d7d49b643692b
Myron <josephonyekachi77@gmail.com>
2023-01-03 17:44:39 -0800

    updated

--- app/database/connection.php
+++ app/database/connection.php
@@ -6,13 +6,5 @@ $db_name = 'portfolio';
 
 $conn = new mysqli($host, $user, $pass, $db_name);
 
-// checks if the connection was successfull
-
-// if ($conn->connect_error) {
-//     die('Database connection error: '. $conn->connect_error);
-// }
-// else{
-//     echo 'No Error detected';
-// }
 
 ?>
